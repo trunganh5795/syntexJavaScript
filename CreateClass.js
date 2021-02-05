019b3f2d1490e40aab6859841ec27cc8b3c033da
trunganh5795 <75206392+trunganh5795@users.noreply.github.com>
2021-02-05 15:42:28 +0700

    Create CreateClass.js

---
+++ CreateClass.js
@@ -0,0 +1,12 @@
+export class Glasses {
+    // nhớ là dấu chấm phẩy
+    id = '';
+    name = '';
+    brand = '';
+    constructor() {
+        // hàm khởi tạo
+    }
+    changeGlasses(id) {
+        this.id = id
+    }
+}
